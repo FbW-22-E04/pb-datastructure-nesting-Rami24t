47c7d5e3d60fab78f4091eb780b3c08633ce9403
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-11 15:49:41 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -13,4 +13,16 @@ console.log(item2)
 }
 
 //2.
+doggo = {name: 'Ramzi', breed: 'Kanisch', "favorite foods": ['batata', 'burger'],
+printFavoriteFoods(){
+for(food of doggo["favorite foods"])
+console.log(food);
+}
+};
+
+
+console.log('2.3',doggo["favorite foods"][1]);
+doggo.printFavoriteFoods();
+
+//3.
 
