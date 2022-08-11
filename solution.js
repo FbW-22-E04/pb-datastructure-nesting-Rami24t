dfc446e062a2524952c407c689c8b8b1d66a2ad0
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-11 15:43:09 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,16 @@
+//1.
+let board = [
+  [1, 2, 3],
+  ["quick", "brown", "fox", "jumped", "over", "lazy", "dog"],
+  [true, false]
+];
+
+for(item of board){
+console.log(item)
+for(item2 of item){
+console.log(item2)
+}
+}
+
+//2.
+
